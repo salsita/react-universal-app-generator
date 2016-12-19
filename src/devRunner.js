698e978da35187b4bb3e7e7eda804301f3564043
Tomas Weiss <tomasw@salsitasoft.com>
2016-12-19 14:56:20 +0100

    formatting errors & warnings from webpack compilation

--- src/devRunner.js
+++ src/devRunner.js
@@ -1,5 +1,6 @@
 import spawn from 'cross-spawn';
 import webpack from 'webpack';
+import formatWebpackMessages from 'react-dev-utils/formatWebpackMessages';
 
 import buildServerWebpackConfig from './buildServerWebpackConfig';
 
@@ -18,10 +19,26 @@ export default serverRelativePath => {
       process.exit(1);
     }
 
-    const jsonStats = stats.toJson();
-    console.log(jsonStats.errors);
-    console.log(jsonStats.warnings);
-    console.log('compiled');
+    const messages = formatWebpackMessages(stats.toJson({}, true));
+
+    if (messages.errors.length) {
+      console.log('Failed to compile');
+      console.log();
+      messages.errors.forEach(message => {
+        console.log(message);
+        console.log();
+      });
+      return;
+    }
+
+    if (messages.warnings.length) {
+      console.log('Compiled with warnings.');
+      console.log();
+      messages.warnings.forEach(message => {
+        console.log(message);
+        console.log();
+      });
+    }
 
     if (!serverProcess) {
       serverProcess = spawn.spawn(
